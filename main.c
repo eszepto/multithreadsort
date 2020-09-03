5f871929c8eea4caf513003514a4c0c1171cf402
Pichet <bestoinw@hotmail.com>
2020-09-04 03:41:15 +0700

    add merge function to merge after sorting

--- main.c
+++ main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-int list[] = {92,45,10,13,51,75,25,46,90,44,35,8};
+static int list[] = {92,45,10,13,51,75,25,46,90,44,35,8, 3};
+int listLength = sizeof(list)/sizeof(int);
+
 void *SortList(void *param);
 void *MergeList(void *param);
 void PrintList();
-typedef struct data{
+typedef struct args{
     int* arr;
     int start;
     int stop;
-} data;
+} args;
 /* C implementation QuickSort */
 // A utility function to swap two elements
 void swap(int* a, int* b)
@@ -63,15 +65,13 @@ void quickSort(int arr[], int low, int high)
 
 int main(int argc, char *argv[])
 {
-    int listLength = sizeof(list)/sizeof(int);
     int firstStart = 0;
     int firstStop = listLength/2 - 1;
     int secondStart = listLength/2;
     int secondStop = listLength - 1;
 
     pthread_t sortThread0, sortThread1, mergeThread0;
-    data args1;
-    data args2;
+    args args1, args2;
 
     args1.start = firstStart;
     args1.stop = firstStop;
@@ -82,22 +82,66 @@ int main(int argc, char *argv[])
     pthread_create(&sortThread0, NULL, SortList, &args1);
     pthread_create(&sortThread1, NULL, SortList, &args2);
 
-    pthread_join(sortThread0,NULL);
-    pthread_join(sortThread1,NULL);
+    pthread_join(sortThread0, NULL);
+    pthread_join(sortThread1, NULL);
+
+    pthread_create(&mergeThread0, NULL, MergeList, NULL);
+    pthread_join(mergeThread0, NULL);
 
     PrintList();
 }
 
 void *SortList(void *param)
-{   data* ptr = (data*)param;
+{
+    args* ptr = (args*)param;
     int start = ptr->start;
     int stop = ptr->stop;
     quickSort(list, start, stop);
     pthread_exit(0);
-
 }
 void *MergeList(void *param)
 {
+
+    int firstStart = 0;
+    int firstStop = listLength/2 - 1;
+    int secondStart = listLength/2;
+    int secondStop = listLength - 1;
+
+    int temp[listLength];
+
+    int runner1 = firstStart;
+    int runner2 = secondStart;
+
+    int i = 0;
+    while(i<listLength)
+    {
+        if(runner1 > firstStop)
+        {
+            temp[i] = list[runner2];
+            runner2 += 1;
+        }
+        else if(runner2 > secondStop)
+        {
+            temp[i] = list[runner1];
+            runner1 += 1;
+        }
+        else if(list[runner1] < list[runner2])
+        {
+            temp[i] = list[runner1];
+            runner1 += 1;
+        }
+        else
+        {
+            temp[i] = list[runner2];
+            runner2 += 1;
+        }
+
+        i++;
+    };
+    for(int i=0;i<listLength;i++)
+    {
+        list[i] = temp[i];
+    }
 
 }
 
